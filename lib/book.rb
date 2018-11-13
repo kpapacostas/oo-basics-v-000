d76df99a64685a5001defc498e0f65e45dbec907
Katerina  Papacostas <katpapacostas@gmail.com>
2018-11-13 15:37:38 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -9,35 +9,6 @@ class Book
     @title = title 
   end
   
-  # def title 
-  #   @title
-  # end 
-  
-  # def author=(author_name)
-  #   @author = author_name
-  #   @author
-  # end 
-  
-  # def author
-  #   @author
-  # end 
-  
-  def page_count=(page_count)
-    @page_count = page_count
-    # @page_count
-  end 
-  
-  def page_count
-    @page_count
-  end 
-  
-  def genre=(genre)
-    @genre = genre 
-    # @genre
-  end 
-  
-  def genre
-    @genre
-  end
+ 
   
 end 
